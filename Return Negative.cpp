db1b1a012cab8d08f9f7d19e3a8843445cc9b2e5
nkhachatryan <44493849+nkhachatryan@users.noreply.github.com>
2018-11-14 12:16:41 +0300

    Update Return Negative.cpp

--- Return Negative.cpp
+++ Return Negative.cpp
@@ -1,5 +1,5 @@
 //Task:
-//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+//https://www.codewars.com/kata/55685cd7ad70877c23000102
 //Solution:
 int makeNegative(int a)
 {
