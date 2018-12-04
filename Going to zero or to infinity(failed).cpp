e221f66785277dd0f40e40525df99cb1f8a71fd0
nkhachatryan <44493849+nkhachatryan@users.noreply.github.com>
2018-12-04 16:43:12 +0300

    Update Going to zero or to infinity(failed).cpp

--- Going to zero or to infinity(failed).cpp
+++ Going to zero or to infinity(failed).cpp
@@ -8,15 +8,19 @@ class Suite
 public:
   static double going(int n);
 };
-inline double Suite :: going(int n){
-long double ans;
-long double fac=1.0;
-long double sum=1.0;
-for(int i=2; i<=n; i++){
-fac=fac*i;
-sum=sum+fac;
-}
-ans=sum/fac;
-ans=( (float) (( (int) ((ans+0.000005)*1000000))/1000000.0));
-return ans;
+inline double Suite :: going(int n)
+{
+  long double ans;
+  long double fac=1.0;
+  long double sum=1.0;
+  
+  for(int i=2; i<=n; i++)
+  {
+    fac=fac*i;
+    sum=sum+fac;
+  }
+   ans=sum/fac;
+   ans=( (float) (( (int) ((ans+0.000005)*1000000))/1000000.0));
+  
+    return ans;
 }
